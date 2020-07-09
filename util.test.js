3bbbbd930865c069aedd189cf4332427256d395e
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-09 08:47:38 +0530

    Check against false positivity

--- util.test.js
+++ util.test.js
@@ -1,6 +1,13 @@
 const { generateText } = require('./util')
 
+// Unit test
 test('should op a name and age', () => {
 	const text = generateText('Bisu', 20)
 	expect(text).toBe('Bisu (20 years old)')
+})
+
+// Unit test for ruling out false positivity
+test('should op with empty inputs', () => {
+	const text = generateText('', null)
+	expect(text).toBe(' (null years old)')
 })
