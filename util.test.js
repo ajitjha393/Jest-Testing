6de6ce6e1efb6502ac7b20dba2194ab41be5ab12
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-09 09:03:33 +0530

    Integration Test for checkAndGenerate

--- util.test.js
+++ util.test.js
@@ -1,4 +1,4 @@
-const { generateText } = require('./util')
+const { generateText, checkAndGenerate } = require('./util')
 
 // Unit test
 test('should op a name and age', () => {
@@ -10,4 +10,11 @@ test('should op a name and age', () => {
 test('should op with empty inputs', () => {
 	const text = generateText('', null)
 	expect(text).toBe(' (null years old)')
+})
+
+// Integration test
+
+test('should output valid input value', () => {
+	const text = checkAndGenerate('Aju', 20)
+	expect(text).toBe('Aju (20 years old)')
 })
